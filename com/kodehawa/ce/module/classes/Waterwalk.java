d2eb82efec803a014b3ec103196273ad63a4e977
David Alejandro Rubio Escares <kodehawa12@hotmail.com>
2013-08-29 07:03:57 -0400

    Waterwalk fix

--- com/kodehawa/ce/module/classes/Waterwalk.java
+++ com/kodehawa/ce/module/classes/Waterwalk.java
@@ -1,13 +1,7 @@
 package com.kodehawa.ce.module.classes;
 
-import net.minecraft.client.entity.EntityClientPlayerMP;
-import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.entity.EntityLivingBase;
-
 import org.lwjgl.input.Keyboard;
 
-import com.kodehawa.ce.CheatingEssentials;
-import com.kodehawa.ce.api.reflection.ReflectorHelper;
 import com.kodehawa.ce.module.core.CheatingEssentialsModule;
 import com.kodehawa.ce.module.enums.EnumGuiCategory;
 
@@ -17,24 +11,18 @@ public class Waterwalk extends CheatingEssentialsModule {
 		super("Water Walk", "", "1.6.2", Keyboard.KEY_J, EnumGuiCategory.WORLD, true);
         super.setTick(true);
 	}
-
+	
+	//This crap works like a shit. But works
 	@Override
 	public void tick() {
 		if (getPlayer().isInWater())
         {
+			getMinecraft().gameSettings.keyBindJump.pressed = true;
             getPlayer().setSprinting(false);
-            try{
-                for(Object o : getMinecraft().theWorld.loadedEntityList)  {
-                    if(o instanceof EntityPlayerSP || o instanceof EntityClientPlayerMP){
-                       ReflectorHelper.getPrivateMethod(EntityLivingBase.class, o, "jump" );
-                    }
-                }
-            }
-            catch (Exception e){
-                CheatingEssentials.CELogAgent("First failures in reflection code.");
-            }
-            getPlayer().motionY /= 2;
         } 
+		else{
+			getMinecraft().gameSettings.keyBindJump.pressed = false;
+		}
 	}
 
 	@Override
@@ -44,5 +32,6 @@ public class Waterwalk extends CheatingEssentialsModule {
 	@Override
 	public void onDisableModule() {
 	}
+
 
 }
